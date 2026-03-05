7e34bc75dcc1d0da71ebd3df43d1df1a51a925f6
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-06 01:48:50 +0300

    VKE-1 first task

--- app/src/main/java/dev/toothlonely/vkeducation/MainActivity.kt
+++ app/src/main/java/dev/toothlonely/vkeducation/MainActivity.kt
@@ -1,15 +1,28 @@
 package dev.toothlonely.vkeducation
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import dev.toothlonely.vkeducation.ui.theme.VKEducationTheme
 
@@ -20,8 +33,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             VKEducationTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
+                    FirstScreen(
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
@@ -31,17 +43,50 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun FirstScreen(modifier: Modifier = Modifier) {
+
+    var text by remember { mutableStateOf("") }
+    val context = LocalContext.current
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize()
+    ) {
+        OutlinedTextField(
+            value = text,
+            onValueChange = {
+                text = it
+            },
+            label = {
+                Text(text = stringResource(R.string.text_for_second_activity))
+            },
+            placeholder = {
+                Text(text = stringResource(R.string.edit_text_placeholder))
+            }
+        )
+
+        Button(
+            onClick = {
+                openSecondActivity(context, text)
+            }
+        ) {
+            Text("Открыть вторую Activity")
+        }
+    }
+}
+
+private fun openSecondActivity(context: Context, message: String) {
+    val intent = Intent(context, SecondActivity::class.java).apply {
+        putExtra("secondActivityMessage", message)
+    }
+    context.startActivity(intent)
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun FirstScreenPreview() {
     VKEducationTheme {
-        Greeting("Android")
+        FirstScreen()
     }
 }
--- app/src/main/java/dev/toothlonely/vkeducation/SecondActivity.kt
+++ app/src/main/java/dev/toothlonely/vkeducation/SecondActivity.kt
@@ -1,4 +1,49 @@
 package dev.toothlonely.vkeducation
 
-class SecondActivity {
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import dev.toothlonely.vkeducation.ui.theme.VKEducationTheme
+
+class SecondActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            VKEducationTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    val intent = intent
+                    SecondScreen(
+                        intent = intent,
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun SecondScreen(intent: Intent, modifier: Modifier = Modifier) {
+
+    val text = intent.getStringExtra("secondActivityMessage") ?: ""
+
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier.fillMaxSize(),
+    ) {
+        Text(text = text)
+    }
 }
