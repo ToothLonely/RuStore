733bfd4dce6319adc11c0cbb900a38d0dfb42bd7
Tooth Lonely <arsen.shirinov2004@gmail.com>
2026-03-12 18:28:57 +0300

    VKE-2 added navigation

---
+++ app/src/main/java/dev/toothlonely/vkeducation/Navigation.kt
@@ -0,0 +1,40 @@
+package dev.toothlonely.vkeducation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.toRoute
+import kotlinx.serialization.Serializable
+
+sealed interface Screen {
+
+    @Serializable
+    data object AppsList : Screen
+
+    @Serializable
+    data class AppInfo(val applicationId: Int) : Screen
+}
+
+@Composable
+fun Navigation(
+    navController: NavHostController,
+    modifier: Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = Screen.AppsList
+    ) {
+        composable<Screen.AppsList> {
+            AppsListScreen(modifier = modifier) { destination ->
+                navController.navigate(destination)
+            }
+        }
+
+        composable<Screen.AppInfo> { backStackEntry ->
+            val appItem = backStackEntry.toRoute<Screen.AppInfo>()
+            AppInfoScreen(applicationId = appItem.applicationId)
+        }
+    }
+}
